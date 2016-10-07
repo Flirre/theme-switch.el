bf80556add5b8733d31d60fda3b8a551058051a0
Filip Lindahl <gysztaf@gmail.com>
2016-10-07 09:02:33 +0200

    progress?

--- theme-switch.el
+++ theme-switch.el
@@ -5,14 +5,24 @@
 "This function gets Json data from \"http://api.sunrise-sunset.org/json\". It
 is their API that I am using for this little program."
  (request
- "http://api.sunrise-sunset.org/json?";
+ "http://api.sunrise-sunset.org/json?lat=57.70887&lng=11.974560&date=today"
  :params '(("lat" . "57.70887") ("lng" . "11.974560") ("date" . "today"))
  :parser 'json-read
- :success (function*
+ :success (cl-function
            (lambda (&key data &allow-other-keys)
              (message "response: %S" data))))
  )
 
+
+;(request
+; "http://api.sunrise-sunset.org/json?lat=57.70887&lng=11.974560&date=today"
+; :parser 'json-read
+; :success (cl-function
+;           (lambda (&key data &allow-other-keys)
+;             (message "response: %S" data))))
+
 (getJsonSunData)
 
-(defvar jsonData (getJsonSunData))
+;(defvar jsonData getJsonSunData)
+;(jsonData)
+;(assoc 'results jsonData())
