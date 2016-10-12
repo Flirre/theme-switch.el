4d1838a02412f9d7f06c1cf01c66f331aaf5e6bb
Filip Lindahl <gysztaf@gmail.com>
2016-10-12 22:04:39 +0200

    Fixed formatting and also broke stuff.

--- theme-switch.el
+++ theme-switch.el
@@ -6,15 +6,19 @@
 is their API that I am using for this little program."
  (request
  "http://api.sunrise-sunset.org/json"
- :params '(("lat" . "57.70887") ("lng" . "11.974560") ("date" . "today"))
+ :params '(("lat" . "57.70887") ("lng" . "11.974560") ("date" . "today") ("formatted" . "0"))
  :parser 'json-read
  :success (cl-function
            (lambda (&key data &allow-other-keys)
              (setq jsonData data)))) ;Credits to @Jassob.
  )
 
-(getJsonSunData)
-(setq jsonData (getJsonSunData))
-(setq sunrise (cdr (assoc 'sunrise (assoc 'results jsonData))))
-(setq sunset (cdr (assoc 'sunset (assoc 'results jsonData))))
-(setq current (format-time-string "%-l:%M:%S %p" (current-time)))
+(defun sunnyout()
+  "Checks if the current time is during sun-hours or not, in Gothenburg"
+  (
+   (setq jsonData (getJsonSunData))
+   (setq sunrise (date-to-time(cdr (assoc 'sunrise (assoc 'results jsonData)))))
+   (setq sunset (date-to-time(cdr (assoc 'sunset (assoc 'results jsonData)))))
+
+   (when (time-less-p sunrise (current-time))(time-less-p (current-time) sunset))
+ )
